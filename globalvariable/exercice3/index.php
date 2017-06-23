32bc2921e1ab05ef70427437a7498d5ccaa53e18
Romain32190 <romain.lafargue36@gmail.com>
2017-06-23 10:36:19 +0200

    Ajout exercice 3

---
+++ globalvariable/exercice3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form  action="user.php" method="POST">
+      <label for="login"></label>
+      <input type="text" name="login"></input>
+      <label for="password"></label>
+      <input type="password" name="password"></input>
+      <button type="submit">Connectez vous</button>
+
+
+    </form>
+
+  </body>
+</html>
