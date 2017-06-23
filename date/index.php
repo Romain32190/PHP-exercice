c8a88c51177f1410bef42c305c0f7bfd01a748c1
Romain32190 <romain.lafargue36@gmail.com>
2017-06-23 14:32:37 +0200

    Exercice 4

--- date/index.php
+++ date/index.php
@@ -1,7 +1,22 @@
 <?php
 
+
+print_r('Exercice1 :');
 echo date('d-m-Y');
+
+
+print_r('Exercice2 : ');
 echo date('d-m-y');
 
 
- ?>
+print_r('Exercice3 : ');
+echo date('l j F Y');
+setlocale (LC_TIME, 'fr_FR.utf8','fra');
+echo (strftime("%A %d %B %Y"));
+
+print_r('Exercice4 : ');
+echo time();
+echo mktime(15,0,0,8,2,2016);
+
+
+?>
